4e6052957e398d70117baa05525eaf4db8107e3b
griffi-gh <prasol258@gmail.com>
2023-01-15 18:05:16 +0100

    Use `Core` profile

--- src/game/display.rs
+++ src/game/display.rs
@@ -1,12 +1,15 @@
 use glium::Display;
 use glium::glutin::{
-  ContextBuilder, 
+  ContextBuilder,
+  GlProfile,
   window::WindowBuilder,
   event_loop::EventLoop
 };
 
 pub fn init_display(event_loop: &EventLoop<()>) -> Display {
     let wb = WindowBuilder::new();
-    let cb = ContextBuilder::new().with_depth_buffer(24);
+    let cb = ContextBuilder::new()
+      .with_depth_buffer(24)
+      .with_gl_profile(GlProfile::Core);
     Display::new(wb, cb, event_loop).unwrap()
 }
