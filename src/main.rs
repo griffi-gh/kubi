b1b78cc4dd637950fa41152f1dfc67e438f01475
griffi-gh <prasol258@gmail.com>
2023-01-19 21:58:49 +0100

    x

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
+use glium::glutin::event_loop::EventLoop;
 use shipyard::World;
 
+mod logging;
+
 fn main() {
+  logging::init();
+  
   let world = World::new();
+  world.add_unique(component)
+
+  let event_loop = EventLoop::new();
+  event_loop.run(move |event, _, control_flow| {
+    
+  });
 }
