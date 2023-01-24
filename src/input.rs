9c802a6e70a3d3bf66acc4007513ae300f1226b6
griffi-gh <prasol258@gmail.com>
2023-01-24 19:09:41 +0100

    input system updates

--- src/input.rs
+++ src/input.rs
@@ -3,17 +3,20 @@ use shipyard::{AllStoragesView, Unique};
 use hashbrown::HashMap;
 use nohash_hasher::BuildNoHashHasher;
 
-#[repr(u8)]
 #[derive(Clone, Copy, Debug)]
+#[repr(u8)]
 pub enum Action {
   Move(f32, f32),
   Look(f32, f32),
 }
 
+pub type MouseCallback<A> = fn(x: f32, y: f32) -> A;
+
 #[derive(Unique)]
 pub struct InputSystem<A: 'static> {
   pub keyboard_keymap: HashMap<VirtualKeyCode, A, BuildNoHashHasher<u32>>,
-  mouse_map: Option<fn(f32, f32) -> A>,
+  mouse_map: Option<MouseCallback<A>>,
+  pub mouse_sensitivity: f32,
   keyboard_state: HashMap<VirtualKeyCode, bool, BuildNoHashHasher<u32>>,
   mouse_delta: (f32, f32),
   mouse_position: (f32, f32),
@@ -23,12 +26,13 @@ impl<A> InputSystem<A> {
     Self {
       keyboard_keymap: HashMap::with_hasher(BuildNoHashHasher::default()),
       mouse_map: None,
+      mouse_sensitivity: 1.,
       keyboard_state: HashMap::with_hasher(BuildNoHashHasher::default()),
       mouse_delta: (0., 0.),
       mouse_position: (0., 0.),
     }
   }
-  pub fn map_to_mouse(&mut self, function: fn(f32, f32) -> A) {
+  pub fn map_to_mouse(&mut self, function: MouseCallback<A>) {
     self.mouse_map = Some(function);
   }
 }
