b4462a0176c678770891bf468716933d623fc30a
griffi-gh <prasol258@gmail.com>
2023-01-28 04:26:47 +0100

    wip

--- src/rendering/selection_box.rs
+++ src/rendering/selection_box.rs
@@ -1,10 +1,12 @@
 use shipyard::{View, IntoIter, NonSendSync, UniqueViewMut};
+use glium::Surface;
 use crate::{
-  player::MainPlayer, 
   world::raycast::LookingAtBlock, 
   camera::Camera
 };
 use super::RenderTarget;
+
+const CUBE_VERTICES: &[f32] = &[0.0];
 
 //wip
 pub fn render_selection_box(
@@ -13,6 +15,17 @@ pub fn render_selection_box(
   mut target: NonSendSync<UniqueViewMut<RenderTarget>>, 
 ) {
   for lookat in lookat.iter() {
-    
+    // target.0.draw(
+    //   &mesh.vertex_buffer,
+    //   &mesh.index_buffer,
+    //   &program.0,
+    //   &uniform! {
+    //     position_offset: world_position.to_array(),
+    //     view: view,
+    //     perspective: perspective,
+    //     tex: texture_sampler,
+    //   },
+    //   &draw_parameters
+    // ).unwrap();
   }
 }
