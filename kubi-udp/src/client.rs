e6ec24a55cd895c77f2f6b10d543b498089cecad
griffi-gh <prasol258@gmail.com>
2023-02-01 03:24:06 +0100

    client

--- kubi-udp/src/client.rs
+++ kubi-udp/src/client.rs
@@ -5,31 +5,35 @@ use std::{
 use bincode::{Encode, Decode};
 use crate::{BINCODE_CONFIG, packet::ClientPacket};
 
-pub struct Client<T> where T: Encode + Decode {
+pub struct Client<S, R> where S: Encode + Decode, R: Encode + Decode {
   socket: UdpSocket,
-  _marker: PhantomData<T>
+  _s: PhantomData<S>,
+  _r: PhantomData<R>,
 }
-impl<T> Client<T> where T: Encode + Decode {
+impl<S, R> Client<S, R> where S: Encode + Decode, R: Encode + Decode {
   pub fn connect(addr: SocketAddr) -> anyhow::Result<Self> {
     let bind_addr: SocketAddr = "127.0.0.1:0".parse().unwrap();
     let socket = UdpSocket::bind(bind_addr)?;
     socket.set_nonblocking(true)?;
     socket.connect(addr)?;
-    Ok(Self {
+    let client = Self {
       socket,
-      _marker: PhantomData
-    })
+      _s: PhantomData,
+      _r: PhantomData,
+    };
+    client.send_packet(&ClientPacket::Connect)?;
+    Ok(client)
   }
-  fn send_packet(&self, packet: &ClientPacket<T>) -> anyhow::Result<()> {
+  fn send_packet(&self, packet: &ClientPacket<S>) -> anyhow::Result<()> {
     let bytes = bincode::encode_to_vec(packet, BINCODE_CONFIG)?;
     self.socket.send(&bytes)?;
     Ok(())
   }
-  pub fn send(&self, message: T) -> anyhow::Result<()> {
+  pub fn send_message(&self, message: S) -> anyhow::Result<()> {
     self.send_packet(&ClientPacket::Data(message))?;
     Ok(())
   }
-  pub fn disconnect(&self) -> anyhow::Result<()> {
+  pub fn disconnect(self) -> anyhow::Result<()> {
     self.send_packet(&ClientPacket::Disconnect)?;
     Ok(())
   }
