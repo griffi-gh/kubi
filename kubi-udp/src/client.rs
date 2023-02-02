99b2848775c1ee75bb00683e23610cab24a7bb21
griffi-gh <prasol258@gmail.com>
2023-02-02 01:40:48 +0100

    client

--- kubi-udp/src/client.rs
+++ kubi-udp/src/client.rs
@@ -1,41 +1,54 @@
 use std::{
   net::{UdpSocket, SocketAddr},
-  marker::PhantomData
+  marker::PhantomData, time::Instant
 };
 use bincode::{Encode, Decode};
 use crate::{BINCODE_CONFIG, packet::ClientPacket};
 
+#[derive(Clone, Copy, Debug)]
+pub struct ClientConfig {
+  
+}
+
 pub struct Client<S, R> where S: Encode + Decode, R: Encode + Decode {
+  addr: SocketAddr,
+  config: ClientConfig,
   socket: UdpSocket,
+  last_heartbeat: Instant,
   _s: PhantomData<*const S>,
   _r: PhantomData<*const R>,
 }
 impl<S, R> Client<S, R> where S: Encode + Decode, R: Encode + Decode {
-  pub fn connect(addr: SocketAddr) -> anyhow::Result<Self> {
+  pub fn connect(addr: SocketAddr, config: ClientConfig) -> anyhow::Result<Self> {
     let bind_addr: SocketAddr = "127.0.0.1:0".parse().unwrap();
     let socket = UdpSocket::bind(bind_addr)?;
     socket.set_nonblocking(true)?;
     socket.connect(addr)?;
     let client = Self {
+      addr,
+      config,
       socket,
+      last_heartbeat: Instant::now(),
       _s: PhantomData,
       _r: PhantomData,
     };
-    client.send_packet(&ClientPacket::Connect)?;
+    client.send_raw_packet(&ClientPacket::Connect)?;
     Ok(client)
   }
-  //maybe move generics here if possible?
-  fn send_packet(&self, packet: &ClientPacket<S>) -> anyhow::Result<()> {
+  fn send_raw_packet(&self, packet: &ClientPacket<S>) -> anyhow::Result<()> {
     let bytes = bincode::encode_to_vec(packet, BINCODE_CONFIG)?;
     self.socket.send(&bytes)?;
     Ok(())
   }
   pub fn send_message(&self, message: S) -> anyhow::Result<()> {
-    self.send_packet(&ClientPacket::Data(message))?;
+    self.send_raw_packet(&ClientPacket::Data(message))?;
     Ok(())
   }
   pub fn disconnect(self) -> anyhow::Result<()> {
-    self.send_packet(&ClientPacket::Disconnect)?;
+    self.send_raw_packet(&ClientPacket::Disconnect)?;
     Ok(())
+  }
+  pub fn update(&mut self) {
+    
   }
 }
--- kubi-udp/src/lib.rs
+++ kubi-udp/src/lib.rs
@@ -5,6 +5,7 @@ pub(crate) mod packet;
 pub use server::Server;
 pub use client::Client;
 
+//pub(crate) trait Serializable: bincode::Encode + bincode::Decode {}
 pub(crate) const BINCODE_CONFIG: bincode::config::Configuration<bincode::config::LittleEndian, bincode::config::Varint, bincode::config::SkipFixedArrayLength> = bincode::config::standard()
   .with_little_endian()
   .with_variable_int_encoding()
