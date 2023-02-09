6b34352c5529f8f500fd3a262580281b6aa3ce25
griffi-gh <prasol258@gmail.com>
2023-02-09 03:33:41 +0100

    fix color-related code

--- kubi/src/color.rs
+++ kubi/src/color.rs
@@ -6,7 +6,7 @@ pub fn color_rgba(r: u8, g: u8, b: u8, a: u8) -> Vec4 {
 }
 
 #[inline(always)]
-pub const fn color_hex(c: u32) -> Vec4 {
+pub fn color_hex(c: u32) -> Vec4 {
   let c = c.to_le_bytes();
-  vec4(c[0] as f32, c[1] as f32, c[2] as f32, c[3] as f32)
+  color_rgba(c[0], c[1], c[2], c[3])
 }
