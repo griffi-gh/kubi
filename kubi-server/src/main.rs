28a82cf69efd2fe6cc6af1716eb906f7d0e956e1
griffi-gh <prasol258@gmail.com>
2023-02-10 20:26:03 +0100

    server

--- kubi-server/src/main.rs
+++ kubi-server/src/main.rs
@@ -1,3 +1,14 @@
+use kubi_udp::server::{Server, ServerConfig};
+use kubi_shared::networking::messages::{ClientToServerMessage, ServerToClientMessage};
+
 fn main() {
-  
+  let mut server: Server<ServerToClientMessage, ClientToServerMessage> = Server::bind(
+    "0.0.0.0:1234".parse().unwrap(), 
+    ServerConfig::default()
+  ).unwrap();
+  loop {
+    if let Err(or) = server.update() {
+      println!("Server error: {or:?}")
+    }
+  }
 }
