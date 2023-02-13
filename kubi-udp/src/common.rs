b3990e64c9f4a5825ec088f5b0864d7842ee564f
griffi-gh <prasol258@gmail.com>
2023-02-13 01:56:14 +0100

    change default protocol id

--- kubi-udp/src/common.rs
+++ kubi-udp/src/common.rs
@@ -5,4 +5,4 @@ pub type ClientIdRepr = u8;
 pub const MAX_CLIENTS: usize = u8::MAX as _;
 
 pub const PROTOCOL_ID: u16 = 1;
-pub const DEFAULT_USER_PROTOCOL_ID: u16 = 0xcafe;
+pub const DEFAULT_USER_PROTOCOL_ID: u16 = 0xffff;
