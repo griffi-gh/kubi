356455a15eb70fce2c41448252d4e2ff05f79fc0
griffi-gh <prasol258@gmail.com>
2023-05-21 12:29:01 +0200

    remove geom shader macro

--- kubi/src/prefabs/shaders.rs
+++ kubi/src/prefabs/shaders.rs
@@ -1,18 +1,4 @@
-
-
 macro_rules! include_shader_prefab {
-  ($name: literal, $vert: literal, $frag: literal, $geom: literal, $facade: expr) => {
-    { 
-      use ::glium::Program;
-      log::info!("compiling shader {}", $name);
-      Program::from_source(
-        &*$facade,
-        include_str!($vert),
-        include_str!($frag),
-        Some(include_str!($geom)),
-      ).expect("Failed to compile gpu program")
-    }
-  };
   ($name: literal, $vert: literal, $frag: literal, $facade: expr) => {
     {
       use ::glium::Program;
