9bdc38ccf0cfdc03a895386c09191008707ab612
griffi-gh <prasol258@gmail.com>
2023-11-19 19:30:06 +0100

    wip kubipool

---
+++ kubi-pool/src/lib.rs
@@ -0,0 +1,33 @@
+use std::{thread::JoinHandle, collections::VecDeque};
+
+pub struct KubiPool<T, R> {
+    callback: fn(T) -> R,
+    threads: Vec<JoinHandle<()>>,
+}
+
+struct Task<T> {
+    priority: u8,
+    data: T,
+}
+
+fn task_loop<T, R>() {
+    let tasks = VecDeque::<Task<T>>::new();
+    loop {
+        //todo
+    };
+}
+
+impl<T: 'static, R: 'static> KubiPool<T, R> {
+    pub fn new(threads: usize, callback: fn(T) -> R) -> Self {
+        Self {
+            callback,
+            threads: (0..threads).map(|_| {
+                std::thread::spawn(task_loop::<T, R>)
+            }).collect(),
+        }
+    }
+
+    pub fn resize(&mut self, threads: usize) {
+
+    }
+}
