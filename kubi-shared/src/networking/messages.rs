ff6f89f92eb654b18ecaaeea89c7bf6023371c45
griffi-gh <prasol258@gmail.com>
2023-12-03 16:48:48 +0100

    allow chunk size to be increased past 32

--- kubi-shared/src/networking/messages.rs
+++ kubi-shared/src/networking/messages.rs
@@ -1,10 +1,9 @@
 use glam::{Vec3, IVec3, Quat};
 use serde::{Serialize, Deserialize};
-use crate::{chunk::BlockData, queue::QueuedBlock, entity::Health};
+use crate::{chunk::{BlockData, CHUNK_SIZE}, queue::QueuedBlock, entity::Health};
 use super::client::ClientId;
 
-//protocol id not used yet
-pub const PROTOCOL_ID: u16 = 0; 
+pub const PROTOCOL_ID: u16 = 1;
 
 pub const C_CLIENT_HELLO: u8 = 0;
 pub const C_POSITION_CHANGED: u8 = 1;
@@ -43,35 +42,43 @@ pub const S_QUEUE_BLOCK: u8 = 4;
 pub const S_PLAYER_CONNECTED: u8 = 5;
 pub const S_PLAYER_DISCONNECTED: u8 = 6;
 
+#[serde_with::serde_as]
 #[derive(Serialize, Deserialize, Clone)]
 #[repr(u8)]
 pub enum ServerToClientMessage {
   ServerHello {
     init: InitData
   } = S_SERVER_HELLO,
+
   ServerFuckOff {
     reason: String,
   } = S_SERVER_FUCK_OFF,
+
   PlayerPositionChanged {
     client_id: ClientId,
     position: Vec3,
     direction: Quat,
   } = S_PLAYER_POSITION_CHANGED,
-  ///## WARNING: THIS IS COMPRESSED  
-  ///MESSAGES OF THIS TYPE ARE FULLY 
+
+  ///## WARNING: THIS IS COMPRESSED
+  ///MESSAGES OF THIS TYPE ARE FULLY
   ///COMPRESSED ***EXCEPT THE FIRST BYTE***
   ///TO REDUCE NETWORK USAGE
   ChunkResponse {
     chunk: IVec3,
+    #[serde_as(as = "Box<[[[_; CHUNK_SIZE]; CHUNK_SIZE]; CHUNK_SIZE]>")]
     data: BlockData,
     queued: Vec<QueuedBlock>,
   } = S_CHUNK_RESPONSE,
+
   QueueBlock {
     item: QueuedBlock
   } = S_QUEUE_BLOCK,
+
   PlayerConnected {
     init: ClientInitData
   } = S_PLAYER_CONNECTED,
+
   PlayerDisconnected {
     id: ClientId
   } = S_PLAYER_DISCONNECTED,
