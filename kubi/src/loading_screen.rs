6853f4529ec4e3e1c1f1be4a1a6ffd5b2177cc05
griffi-gh <prasol258@gmail.com>
2024-02-20 22:23:34 +0100

    update `hui` to `0.1.0-alpha.3`, new loading screen

--- kubi/src/loading_screen.rs
+++ kubi/src/loading_screen.rs
@@ -1,4 +1,8 @@
-use hui::element::progress_bar::ProgressBar;
+use hui::{
+  element::{container::Container, progress_bar::ProgressBar, text::Text},
+  layout::{Alignment, UiSize},
+  rectangle::{Corners, Sides}
+};
 use shipyard::{UniqueView, UniqueViewMut, Workload, NonSendSync, IntoWorkload};
 use winit::keyboard::KeyCode;
 use crate::{
@@ -9,20 +13,56 @@ use crate::{
   hui_integration::UiState,
 };
 
-fn render_progressbar(
+fn render_loading_ui(
   mut ui: NonSendSync<UniqueViewMut<UiState>>,
   world: UniqueView<ChunkStorage>,
   size: UniqueView<WindowSize>
 ) {
-  let value = {
-    let loaded = world.chunks.iter().fold(0, |acc, (&_, chunk)| {
-      acc + chunk.desired_state.matches_current(chunk.current_state) as usize
-    });
-    let total = world.chunks.len();
-    loaded as f32 / total as f32
-  };
+  let loaded = world.chunks.iter().fold(0, |acc, (&_, chunk)| {
+    acc + chunk.desired_state.matches_current(chunk.current_state) as usize
+  });
+  let total = world.chunks.len();
+  let value = loaded as f32 / total as f32;
+  let percentage = value * 100.;
   ui.hui.add(
-    ProgressBar { value, ..Default::default() },
+    Container {
+      size: (UiSize::Fraction(1.), UiSize::Fraction(1.)),
+      background: (0.1, 0.1, 0.1, 1. - (value - 0.75).max(0.)).into(),
+      align: Alignment::Center.into(),
+      elements: vec![
+        Box::new(Container {
+          padding: Sides::all(10.),
+          gap: 10.,
+          background: (0.2, 0.2, 0.2).into(),
+          corner_radius: Corners::all(8.),
+          elements: vec![
+            Box::new(Text {
+              text: "Loading...".into(),
+              ..Default::default()
+            }),
+            Box::new(ProgressBar {
+              value,
+              size: (UiSize::Static(400.), UiSize::Auto),
+              corner_radius: Corners::all(2.),
+              ..Default::default()
+            }),
+            Box::new(Container {
+              size: (UiSize::Static(400.), UiSize::Auto),
+              align: (Alignment::End, Alignment::End).into(),
+              elements: vec![
+                Box::new(Text {
+                  text: format!("{loaded}/{total} ({percentage:.1}%)").into(),
+                  ..Default::default()
+                })
+              ],
+              ..Default::default()
+            })
+          ],
+          ..Default::default()
+        })
+      ],
+      ..Default::default()
+    },
     size.0.as_vec2()
   );
 }
@@ -53,7 +93,7 @@ fn override_loading(
 
 pub fn update_loading_screen() -> Workload {
   (
-    render_progressbar,
+    render_loading_ui,
     override_loading,
     switch_to_ingame_if_loaded,
   ).into_sequential_workload()
