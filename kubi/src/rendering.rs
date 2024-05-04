0e22bccbaca9a415573b3732331076523c244d8c
griffi-gh <prasol258@gmail.com>
2024-05-04 17:24:19 +0200

    init wgpu stuff

--- kubi/src/rendering.rs
+++ kubi/src/rendering.rs
@@ -1,3 +1,4 @@
+use pollster::FutureExt;
 use shipyard::{Unique, NonSendSync, UniqueView, UniqueViewMut, View, IntoIter, AllStoragesView};
 use winit::{
   event_loop::ActiveEventLoop,
@@ -19,18 +20,27 @@ pub struct BackgroundColor(pub Vec3);
 
 #[derive(Unique, Clone, Copy)]
 #[repr(transparent)]
+#[deprecated = "use Renderer.size instead"]
+#[allow(deprecated)]
 pub struct WindowSize(pub UVec2);
 
 #[derive(Unique)]
 pub struct Renderer {
   pub window: Window,
+  pub instance: wgpu::Instance,
+  pub surface: wgpu::Surface<'static>,
+  pub device: wgpu::Device,
+  pub queue: wgpu::Queue,
+  pub surface_config: wgpu::SurfaceConfiguration,
+  pub size: PhysicalSize<u32>,
+  // pub depth_texture: wgpu::Texture,
 }
 
 impl Renderer {
   pub fn init(event_loop: &ActiveEventLoop, settings: &GameSettings) -> Self {
     log::info!("initializing display");
 
-    let wb = WindowAttributes::new()
+    let window_attributes = Window::default_attributes()
       .with_title("kubi")
       .with_maximized(true)
       .with_min_inner_size(PhysicalSize::new(640, 480))
@@ -44,6 +54,7 @@ impl Renderer {
           let monitor = event_loop.primary_monitor().or_else(|| {
             event_loop.available_monitors().next()
           });
+
           if let Some(monitor) = monitor {
             log::info!("monitor: {}", monitor.name().unwrap_or_else(|| "generic".into()));
             match fs_settings.mode {
@@ -75,8 +86,58 @@ impl Renderer {
           None
         }
       });
+    let window = event_loop.create_window(window_attributes).unwrap();
 
-    Self { window }
+    let size = window.inner_size();
+
+    let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+      backends: wgpu::Backends::all(),
+      ..Default::default()
+    });
+
+    let surface = instance.create_surface(window).unwrap();
+
+    let adapter = instance.request_adapter(
+      &wgpu::RequestAdapterOptions {
+        power_preference: wgpu::PowerPreference::HighPerformance,
+        compatible_surface: Some(&surface),
+        force_fallback_adapter: false,
+      },
+    ).block_on().unwrap();
+
+    let (device, queue) = adapter.request_device(
+      &wgpu::DeviceDescriptor {
+        label: None,
+        required_features: wgpu::Features::empty(),
+        required_limits: wgpu::Limits::downlevel_defaults(),
+      },
+      None,
+    ).block_on().unwrap();
+
+    let surface_config = surface.get_default_config(&adapter, size.width, size.height).unwrap();
+    surface.configure(&device, &surface_config);
+
+    Self { window, instance, surface, device, queue, surface_config, size }
+  }
+
+  pub fn resize(&mut self, size: PhysicalSize<u32>) {
+    if size.width == 0 || size.height == 0 {
+      log::warn!("Ignoring resize event with zero width or height");
+      return
+    }
+    if self.size == size {
+      log::warn!("Ignoring resize event with same size");
+      return
+    }
+    log::debug!("resizing surface to {:?}", size);
+    self.size = size;
+    self.surface_config.width = size.width;
+    self.surface_config.height = size.height;
+    self.surface.configure(&self.device, &self.surface_config);
+  }
+
+  pub fn reconfigure(&self) {
+    self.surface.configure(&self.device, &self.surface_config);
   }
 }
 
